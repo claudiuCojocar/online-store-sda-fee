c25c1bb864585e8fc0544a0a4030baf52a94e78f
claudiuCojocar <80710920+claudiuCojocar@users.noreply.github.com>
2021-11-02 20:26:34 +0200

    Asd

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/test-application343'))
+app.use(express.static(__dirname + '/dist/online-store-sda-fe'))
 
 app.get('/*', function(request, response) {
-  response.sendFile(path.join(__dirname + '/dist/test-application343/index.html'))
+  response.sendFile(path.join(__dirname + '/dist/online-store-sda-fe/index.html'))
 })
 
 app.listen(process.env.PORT || 4200);
